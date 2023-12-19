a064598aa76cc250747fa1286ad4db5f67b20161
Florivaldo Junior <eucomcerteza@gmail.com>
2023-12-19 11:18:51 -0300

    criação do projeto e estrutura inicial

---
+++ Pedidos/Program.cs
@@ -0,0 +1,23 @@
+﻿using Pedidos.Entities;
+using Pedidos.Entities.Enuns;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Order order = new Order
+        {
+            Id = 1,
+            Moment = DateTime.Now,
+            Status = OrderStatus.PendingPayment
+        };
+
+        Console.WriteLine(order);
+
+        string t1 = OrderStatus.Delivered.ToString() ;
+
+        OrderStatus os = Enum.Parse<OrderStatus>("Delivered");
+
+        Console.WriteLine(os);
+    }
+}
